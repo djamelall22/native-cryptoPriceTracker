3444cfbc38768555578f8864b591ef74be1726e1
djamelall22 <alphamodeling.contact@gmail.com>
2023-01-20 21:43:30 +0100

    add coinDetails header styles

--- src/screens/CoinDetailsScreen/styles.js
+++ src/screens/CoinDetailsScreen/styles.js
@@ -1,11 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  // coin details header
   headerContainer: {
     flexDirection: 'row',
     paddingHorizontal: 10,
     alignItems: 'center',
+    justifyContent: 'space-between',
   },
+
+  tickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  tickerTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginHorizontal: 5,
+    fontSize: 17,
+  },
+
+  rankContainer: {
+    backgroundColor: '#585858',
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    borderRadius: 5,
+  },
+
+  rank: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  // end of coin details header
 });
 
 export default styles;
