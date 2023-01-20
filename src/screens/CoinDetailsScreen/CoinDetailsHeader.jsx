22b09434b9d83eaf86cc8c9bb9df14233945fa0e
djamelall22 <alphamodeling.contact@gmail.com>
2023-01-20 21:43:56 +0100

    add coinDetailsHeader component

---
+++ src/screens/CoinDetailsScreen/CoinDetailsHeader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './styles.js';
+import { View, Image, Text } from 'react-native';
+import { Ionicons, EvilIcons } from '@expo/vector-icons';
+
+const CoinDetailsHeader = ({ symbol, rank, image }) => {
+  return (
+    <View style={styles.headerContainer}>
+      <Ionicons name='chevron-back-sharp' size={30} color='white' />
+      <View style={styles.tickerContainer}>
+        <Image
+          source={{
+            uri: image,
+          }}
+          style={{ width: 25, height: 25 }}
+        />
+        <Text style={styles.tickerTitle}>{symbol.toLocaleUpperCase()}</Text>
+
+        <View style={styles.rankContainer}>
+          <Text style={styles.rank}>#{rank}</Text>
+        </View>
+      </View>
+      <EvilIcons name='user' size={24} color='white' />
+    </View>
+  );
+};
+
+export default CoinDetailsHeader;
