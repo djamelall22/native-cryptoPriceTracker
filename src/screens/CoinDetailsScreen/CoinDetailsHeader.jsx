9ed59fc55679133468aa8b12c873ee961b6f398d
djamelall22 <alphamodeling.contact@gmail.com>
2023-01-20 22:04:46 +0100

    edit import styles link

--- src/screens/CoinDetailsScreen/CoinDetailsHeader.jsx
+++ src/screens/CoinDetailsScreen/CoinDetailsHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles.js';
+import styles from './CoinDetailsHeaderStyles.js';
 import { View, Image, Text } from 'react-native';
 import { Ionicons, EvilIcons } from '@expo/vector-icons';
 
